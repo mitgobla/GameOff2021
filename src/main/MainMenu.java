7680211c4f4b51ad47b0f68c74e8417139b988cc
Emily Canto <56757680+dronade@users.noreply.github.com>
2021-12-05 21:17:44 +0000

    added background
    probably need to re do with css

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -1,22 +1,34 @@
 package main;
 
+import java.io.InputStream;
+import java.io.FileInputStream;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainMenu extends Application {
 
-    public void start(final Stage stage) {
-        stage.setHeight(400);
-        stage.setWidth(700);
+    public void start(final Stage stage) throws IOException{
+        stage.setHeight(640);
+        stage.setWidth(615);
         stage.setTitle("BUG");
 
-        Scene scene = new Scene(new Group());
-        BorderPane root = new BorderPane();
+        InputStream stream = new FileInputStream("src/main/images/galaxy1.png");
+        Image image = new Image(stream);
+        ImageView imageView = new ImageView();
+        imageView.setImage(image);
+        imageView.setX(10);
+        imageView.setY(10);
+        imageView.setFitWidth(575);
+        imageView.setPreserveRatio(true);
 
-        scene.setRoot(root);
+        Group root = new Group(imageView);
+        Scene scene = new Scene(root, 595, 370);
         stage.setScene(scene);
         stage.show();
     }
